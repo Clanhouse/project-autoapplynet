92fae6dc3ef72e9b02c9668e5b528c64c31cc47a
thewilkupl <thewilku@o2.pl>
2021-09-13 23:20:17 +0200

    add header

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -4,6 +4,8 @@ import { validate } from "./validation";
 import styled from "styled-components";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+import { ImFacebook2 } from "react-icons/im";
 
 const Auth = () => {
   const isSignUp = true;
@@ -21,52 +23,31 @@ const Auth = () => {
   });
   return (
     <Wrapper>
-      <form onSubmit={formik.handleSubmit}>
-        {/* <label htmlFor="email">Email</label> */}
+      <div className="main">
+        <h1>autoApply</h1>
+        <form onSubmit={formik.handleSubmit}>
+          {/* <label htmlFor="email">Email</label> */}
+          <Search>
+            <MdEmail
+              style={{ marginLeft: "1rem", position: "absolute" }}
+              color="#f2f4f3"
+              size="1.5em"
+            />
+            <SearchBar
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              placeholder="Enter your email"
+            />
+          </Search>
+          {formik.touched.email && formik.errors.email ? (
+            <Error>{formik.errors.email}</Error>
+          ) : null}
 
-        <Search>
-          <MdEmail
-            style={{ marginLeft: "1rem", position: "absolute" }}
-            color="#f2f4f3"
-            size="1.5em"
-          />
-          <SearchBar
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-            placeholder="Enter your email"
-          />
-        </Search>
-        {formik.touched.email && formik.errors.email ? (
-          <Error>{formik.errors.email}</Error>
-        ) : null}
-
-        {/* <label htmlFor="password">Password</label> */}
-        <Search>
-          <RiLockPasswordFill
-            style={{ marginLeft: "1rem", position: "absolute" }}
-            color="#f2f4f3"
-            size="1.5em"
-          />
-          <SearchBar
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-            placeholder="Enter your password"
-          />
-        </Search>
-        {formik.touched.password && formik.errors.password ? (
-          <Error>{formik.errors.password}</Error>
-        ) : null}
-
-        {/* <label htmlFor="email">Confirm Password</label> */}
-        {isSignUp && (
+          {/* <label htmlFor="password">Password</label> */}
           <Search>
             <RiLockPasswordFill
               style={{ marginLeft: "1rem", position: "absolute" }}
@@ -74,28 +55,57 @@ const Auth = () => {
               size="1.5em"
             />
             <SearchBar
-              id="confirmPassword"
-              name="confirmPassword"
+              id="password"
+              name="password"
               type="password"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.confirmPassword}
-              placeholder="Confirm your password"
+              value={formik.values.password}
+              placeholder="Enter your password"
             />
           </Search>
-        )}
-        {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-          <Error>{formik.errors.confirmPassword}</Error>
-        ) : null}
+          {formik.touched.password && formik.errors.password ? (
+            <Error>{formik.errors.password}</Error>
+          ) : null}
 
-        <button type="submit" className="btn btn-form">
-          Join now!
-        </button>
+          {/* <label htmlFor="email">Confirm Password</label> */}
+          {isSignUp && (
+            <Search>
+              <RiLockPasswordFill
+                style={{ marginLeft: "1rem", position: "absolute" }}
+                color="#f2f4f3"
+                size="1.5em"
+              />
+              <SearchBar
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.confirmPassword}
+                placeholder="Confirm your password"
+              />
+            </Search>
+          )}
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+            <Error>{formik.errors.confirmPassword}</Error>
+          ) : null}
 
-        <div>
-          <p>Or continue with these social profile</p>
-        </div>
-      </form>
+          <button type="submit" className="btn btn-form">
+            Join now!
+          </button>
+        </form>
+        <p style={{ textAlign: "center", color: "#f2f4f3", marginTop: "2rem" }}>
+          Or continue with these social profile
+        </p>
+        <Socials>
+          <FcGoogle className="social-icon" />
+          <ImFacebook2 className="social-icon" style={{ fill: "#3b5998" }} />
+        </Socials>
+        <p style={{ textAlign: "center", color: "#f2f4f3", marginTop: "2rem" }}>
+          Already a member? <button className="btn-switch">Login</button>
+        </p>
+      </div>
     </Wrapper>
   );
 };
@@ -121,15 +131,44 @@ const Wrapper = styled.div`
     }
   }
 
+  .main {
+    display: flex;
+    flex-direction: column;
+    background: #171412;
+    min-width: 25vw;
+    min-height: 65vh;
+    padding: 2rem 2rem;
+    border-radius: 10px;
+    box-shadow: 0rem 1.3rem 1rem rgba(0, 0, 0, 0.7);
+
+    h1 {
+      color: #f2f4f3;
+      text-align: center;
+      font-size: 4rem;
+      transition: all 0.2s;
+
+      &:hover {
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+
+    .btn-switch {
+      border: none;
+      background: transparent;
+      font-family: "Saira", sans-serif;
+      color: #1b998b;
+      font-size: 1rem;
+      cursor: pointer;
+    }
+  }
+
   form {
     display: flex;
     flex-direction: column;
     background: #171412;
-    width: 25%;
-    height: 60%;
-    padding: 2rem 2rem;
-    border-radius: 10px;
-    box-shadow: 0rem 1.3rem 1rem rgba(0, 0, 0, 0.7);
 
     .btn-form {
       border: none;
@@ -137,7 +176,7 @@ const Wrapper = styled.div`
       margin-top: 1rem;
       margin-bottom: 1rem;
       background: #1b998b;
-      color: white;
+      color: #f2f4f3;
       border-radius: 10px;
       font-size: 1rem;
       font-family: "Saira", sans-serif;
@@ -175,13 +214,16 @@ const SearchBar = styled.input`
   border: 1px solid grey;
   transition: all 0.2s;
   font-family: "Saira", sans-serif;
+  transition: all 0.2s ease-in-out;
 
   &::placeholder {
     color: #f2f4f3;
   }
 
   &:focus {
-    background: grey;
+    outline: none;
+    border-bottom: 3px solid #1b998b;
+    border-left: 3px solid #1b998b;
   }
 
   &:focus::placeholder {
@@ -194,6 +236,22 @@ const Error = styled.p`
   font-size: 1rem;
   text-align: left;
   padding: 0rem 1rem;
+  display: inline;
+  transition: all 0.5s;
+`;
+
+const Socials = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem 0rem;
+
+  .social-icon {
+    height: 2.5rem;
+    width: 2.5rem;
+    margin: 0 0.5rem;
+    cursor: pointer;
+  }
 `;
 
 export default Auth;
