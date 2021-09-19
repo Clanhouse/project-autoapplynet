9460e1f794b211b2f35040a28f7df6ac3f0aa7a1
thewilkupl <thewilku@o2.pl>
2021-09-19 17:31:36 +0200

    Create styled.ts to organize styles
    Import all styles to one styled.ts file to avoid many messy files

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-//STYLES
-import styled from "styled-components";
 
-//ICONS
 import { FcGoogle as GoogleIcon} from "react-icons/fc";
 import { ImFacebook2 as FacebookIcon} from "react-icons/im";
 
 import FormFormik from "./FormFormik";
+import { Socials, AuthWrapper } from "./styled";
 
 const Auth: React.FC = () => {
   const [isSignUp, setIsSignUp] = useState(true);
 
   return (
-    <Wrapper>
+    <AuthWrapper>
       <div className="main">
         <h1>
           <Link to="/" className="title">
@@ -44,104 +42,9 @@ const Auth: React.FC = () => {
           </button>
         </p>
       </div>
-    </Wrapper>
+    </AuthWrapper>
   );
 };
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: var(--gradient-primary);
-  background-size: 400% 400%;
-  animation: gradient 15s ease infinite;
-
-  @keyframes gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-
-  .main {
-    display: flex;
-    flex-direction: column;
-    background: #171412;
-    min-width: 25vw;
-    min-height: 65vh;
-    padding: 2rem 2rem;
-    border-radius: 10px;
-    box-shadow: 0rem 1.3rem 1rem rgba(0, 0, 0, 0.7);
-
-    .title {
-      text-decoration: none;
-      color: var(--text-color-primary);
-    }
-
-    h1 {
-      text-align: center;
-      font-size: 4rem;
-      background: transparent;
-      transition: all 0.2s;
-
-      &:hover {
-        background: var(--gradient-primary);
-        -webkit-background-clip: text;
-        background-clip: text;
-        -webkit-text-fill-color: transparent;
-      }
-    }
-
-    h2 {
-      text-align: center;
-      color: var(--text-color-primary);
-    }
-
-    .paragraph {
-      margin-top: 2rem;
-      color: var(--text-color-primary);
-      text-align: center;
-    }
-
-    .btn-switch {
-      border: none;
-      background: transparent;
-      font-family: "Saira", sans-serif;
-      color: var(--color-primary);
-      font-size: 1rem;
-      cursor: pointer;
-      padding-left: 0.3rem;
-    }
-  }
-`;
-
-const Socials = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0.5rem 0rem;
-
-  .social-icon {
-    height: 2.5rem;
-    width: 2.5rem;
-    margin: 0 0.5rem;
-    cursor: pointer;
-    transition: all 0.2s;
-
-    &:hover {
-      transform: translateY(-3px);
-    }
-
-    &:active {
-      transform: translateY(-1px);
-    }
-  }
-`;
 
 export default Auth;
--- frontend/src/components/Auth/FormFormik.tsx
+++ frontend/src/components/Auth/FormFormik.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
-
 import { MdEmail as EmailIcon } from "react-icons/md";
 import {
   AiFillLock as LockIcon,
   AiFillUnlock as UnLockIcon,
 } from "react-icons/ai";
 
-//TYPES
 import {
   FormikValidation,
   FormProps,
   InitialFormValue,
 } from "./FormFormik.types";
+
+import {FormWrapper, Search} from "./styled"
 
 const FormFormik: React.FC<FormProps> = ({ isSignUp }) => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
@@ -22,7 +21,7 @@ const FormFormik: React.FC<FormProps> = ({ isSignUp }) => {
   const initialValues: InitialFormValue = { email: "", password: "" };
 
   return (
-    <Wrapper>
+    <FormWrapper>
       <Formik
         initialValues={initialValues}
         validationSchema={FormikValidation}
@@ -83,100 +82,12 @@ const FormFormik: React.FC<FormProps> = ({ isSignUp }) => {
           </button>
         </Form>
       </Formik>
-    </Wrapper>
+    </FormWrapper>
   );
 };
 
-const Wrapper = styled.div`
-  .form {
-    display: flex;
-    flex-direction: column;
-  }
 
-  .btn-form {
-    border: none;
-    padding: 0.9rem 4rem;
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    background: var(--color-primary);
-    color: var(--text-color-primary);
-    border-radius: 10px;
-    font-size: 1rem;
-    font-family: "Saira", sans-serif;
-    cursor: pointer;
-    transition: all 0.2s;
 
-    &:hover {
-      transform: translateY(-3px);
-    }
 
-    &:active {
-      transform: translateY(-1px);
-    }
-  }
-
-  .input_form {
-    padding: 1rem 1rem 1rem 3.5rem;
-    width: 100%;
-    border-radius: 10px;
-    border: none;
-    background: #171412;
-    color: var(--color-grey);
-    border: 1px solid grey;
-    transition: all 0.2s;
-    font-family: "Saira", sans-serif;
-    transition: all 0.2s ease-in-out;
-
-    &::placeholder {
-      color: var(--text-color-primary);
-    }
-
-    &:focus {
-      outline: none;
-      border-bottom: 3px solid var(--color-primary);
-      border-left: 3px solid var(--color-primary);
-    }
-
-    &:focus::placeholder {
-      color: var(--text-color-primary);
-    }
-  }
-
-  .error_message {
-    color: var(--text-color-primary);
-    font-size: 1rem;
-    padding: 0rem 1rem;
-    display: inline;
-    transition: all 0.5s;
-  }
-
-  .input_icon {
-    color: var(--text-color-primary);
-    height: 1.4rem;
-    width: 1.4rem;
-    position: absolute;
-    margin-left: 1rem;
-  }
-
-  .input_lock {
-    cursor: pointer;
-  }
-
-  .fortgottenPassword {
-    text-align: center;
-    text-decoration: none;
-    color: var(--color-grey);
-  }
-`;
-
-const Search = styled.div`
-  padding: 0.5rem;
-  margin: 0.2rem;
-  position: relative;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  align-self: center;
-`;
 
 export default FormFormik;
